dbe23851c218be57b8c4a7c814cca793bfb03fd9
Sputters <coevyatar@gmail.com>
2018-10-09 21:24:24 +0300

    Add Creation of an IBM MQ Handler

--- cli/fsh_commands.py
+++ cli/fsh_commands.py
@@ -5,18 +5,19 @@ import urllib3
 from config import Config
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+config = Config().config
+auth = (config["credentials"]["username"], config["credentials"]["password"])
 
 @click.command()
 @click.option('--domain', required=True, help='The working domain')
-@click.option('--file', default=None, help='Path to file containing the object values')
 @click.option('--state', type=click.Choice(['enabled','disabled']), default="enabled", help='Set the state of the object', show_default=True)
+@click.option('--file', default=None, help='Path to file containing the object values')
 @click.argument('name')
 @click.argument('listen_address')
 @click.argument('listen_port')
-def create_http_fsh(name, listen_address, listen_port, domain, file, state):
+def create_http_fsh(name, listen_address, listen_port, domain, state, file):
     """ This command creates an HTTP Front side handler """
     click.echo("Creating a new HTTP FSH : {0}".format(name))
-    config = Config().config
     http_fsh_object = {
         "HTTPSourceProtocolHandler" : {
             "AllowedFeatures" : {
@@ -45,11 +46,48 @@ def create_http_fsh(name, listen_address, listen_port, domain, file, state):
         }
     }
     link = str(config["datapower_rest_url"]) + "config/"+ str(domain) +"/HTTPSourceProtocolHandler"
-    auth = (config["credentials"]["username"], config["credentials"]["password"])
     response = requests.post(url=link, data=json.dumps(http_fsh_object), auth=auth, verify=False)
     click.echo("{0} -- {1}".format(response.status_code, response.reason))
 
 @click.command()
-def create_mq_fsh():
+@click.option('--domain', required=True, help='The working domain')
+@click.option('--state', type=click.Choice(['enabled','disabled']), default="enabled", help='Set the state of the object', show_default=True)
+@click.option('--parse-properties', type=click.Choice(['on','off']), default="on", help='Set whether to parse MQ-Headers or not')
+@click.option('--file', default=None, help='Path to file containing the object values')
+@click.option('--async-put', type=click.Choice(['on','off']), default="on", help='Set wether to use AsyncPut or not')
+@click.argument('name')
+@click.argument('queue_manager')
+@click.argument('queue_name')
+def create_mq_fsh(name, queue_manager, queue_name, domain, state, parse_properties, file, async_put):
     """ This command creates an IBM MQ Front sider handler """
-    click.echo("Creating mq fsh")
+    click.echo("Creating a new MQ FSH : {0}".format(name))
+    mq_fsh_object = {
+        "MQSourceProtocolHandler" : {
+            "QueueManager" : str(queue_manager),
+            "GetQueue" : str(queue_name),
+            "CodePage" : 1204,
+            "GetMessageOptions" : 1,
+            "ParseProperties" : str(parse_properties),
+            "AsyncPut" : str(async_put),
+            "ExcludeHeaders" : {
+                "MQCIH" : "off",
+                "MQDLH" : "off",
+                "MQIIH" : "off",
+                "MQRFH" : "off",
+                "MQRFH2" : "off",
+                "MQWIH" : "off"
+            },
+            "ConcurrentConnections" : 1,
+            "PollingInterval" : 30,
+            "BatcjSize" : 0,
+            "ContentTypeHeader" : "None",
+            "RetrieveBackoutSettings" : "off",
+            "UseQMNameInURL" : "off",
+            "mAdminState" : str(state),
+            "name" : str(name)
+        }
+    }
+    click.echo(mq_fsh_object)
+    link = str(config["datapower_rest_url"]) + "config/"+ str(domain) +"/MQSourceProtocolHandler"
+    response = requests.post(url=link, data=json.dumps(mq_fsh_object), auth=auth, verify=False)
+    click.echo("{0} -- {1}".format(response.status_code, response.reason))
