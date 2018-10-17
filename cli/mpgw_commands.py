dfbc57ba43f9fe74c17b1ab7e58aee7a389e07aa
Sputters <coevyatar@gmail.com>
2018-10-17 23:01:04 +0300

    mpgw_commands.py: Finish functionality

--- cli/mpgw_commands.py
+++ cli/mpgw_commands.py
@@ -13,33 +13,26 @@ request_respone_list = ['preprocessed', 'unprocessed', 'xml', 'json', 'soap']
 @click.option('--xml-manager', default='default', help='Set a different xml-manager', show_default=True)
 @click.option('--host-rewrite', type=click.Choice(['on','off']), default='on', help="Set whether to let dp rewrite the 'host' header or not", show_default=True)
 @click.option('--propagate-uri', type=click.Choice(['on','off']), default='on', help="Set whether to pass on the URI from the client to the destination", show_default=True)
-@click.argument('mpgw-name')
-@click.argument('fsh-name')
-@click.argument('backend-url')
-@click.argument('request-type')
-@click.argument('reponse-type')
-def create_mpgw(mpgw_name, fsh_name, backend_url, request_type, response_type, state, xml_manager, host_rewrite, propagate_uri):
-    """ This command creates a Multi-Protocol-Gateway 
-
+@click.option('--backend-url', default='http://localhost:8080', help='The destination URL. Needs to be in the format of <protocol>://<url>/<uri>', show_default=True)
+@click.option('--backend-type', type=click.Choice(['static','dynamic']), default='static', help="Sets either to be a 'static' backend or 'dynamic' backend. 'static' is a single end-point while 'dynamic' is multiple end-points predetermined in the policy", show_default=True)
+@click.argument('mpgw-name', help='The name of the MultiProtocol Gateway')
+@click.argument('fsh-name', help='Either a list or a single name of Front-Side Handlers to add as a refence to the created MPGW')
+@click.argument('request-type', help="The type of the request being sent to the mpgw. must contain a value out of this list :[\'preprocessed\', \'unprocessed\', \'xml\', \'json\', \'soap\']")
+@click.argument('response-type', help="The type of the response being sent from the mpgw. must contain a value out of this list :[\'preprocessed\', \'unprocessed\', \'xml\', \'json\', \'soap\']")
+@click.argument('policy-name', help="The name of the Policy to be attached to this MPGW (Must be an existing Policy)")
+@click.argument('domain-name')
+def create_mpgw(mpgw_name, fsh_name, backend_url, backend_type, request_type, response_type, policy_name, domain_name, state, xml_manager, host_rewrite, propagate_uri):
+    """ This command creates a Multi-Protocol-Gateway
+        
+        Note: The 'backend_type' is an option but it is highly recommended to set a type. In case the 'backend_type' is 'static', use the option 'backend_url' as well or else the default value will be applied instead.
     """
 
-    #    Arguments:
-    #    1) MPGW_NAME    TEXT    "The MultiProtocol Gateway Name"
-    #    2) FSH_NAME     TEXT    "A name of an FSH to be added as a reference to this mpgw"
-    #    3) BACKEND_URL  TEXT    "The destination URL. Needs to be in the format of <protocol>://<url>/<uri>.
-    #                             Example: 'https://localhost:8080/myuri'"
-    #    4) REQUEST_TYPE TEXT    "The type of the request being sent to the mpgw. must contain a value from the list below:
-    #                             ['preprocessed', 'unprocessed', 'xml', 'json', 'soap'] "
-    #    5) RESPONSE_TYPE    TEXT    "The type of the response being sent from the mpgw. must contain a value from the list below:
-    #                             ['preprocessed', 'unprocessed', 'xml', 'json', 'soap'] "
-
-
     if not any(request_type in req for req in request_respone_list):
-        click.secho("The request type is not among the permitted types. Use --help to view possible types.")
+        click.secho("The request type is not among the permitted types. Use --help to view possible types.", fg='red')
     elif not any(response_type in res for res in request_respone_list):
-        click.secho("The response type is not among the permitted types. Use --help to view possible types.")
+        click.secho("The response type is not among the permitted types. Use --help to view possible types.", fg='red')
     else:
-        click.echo("Creating MPGW : {0}".format(mpgw_name))
+        click.secho("Creating MPGW : {0}".format(mpgw_name), fg='yellow')
         mpgw_object = {
             "MultiProtocolGateway": {
                 "name": str(mpgw_name), # Name of the mpgw *required*
@@ -49,7 +42,7 @@ def create_mpgw(mpgw_name, fsh_name, backend_url, request_type, response_type, s
                 }, 
                 "XMLManager": str(xml_manager), # The xml-manager (default default) *required*
                 "BackendUrl": str(backend_url), # The backendUrl *required*
-                "PropagateURI": str(propagate_uri), # Whether to progagate-url or not (default on) *required*
+                "PropagateURI": str(propagate_uri), # Whether to propagate-url or not (default on) *required*
                 "PersistentConnections": "on",
                 "LoopDetection": "off",
                 "DoHostRewriting": str(host_rewrite), # Should datapower rewrite the 'host' header (default on)
@@ -66,12 +59,12 @@ def create_mpgw(mpgw_name, fsh_name, backend_url, request_type, response_type, s
                 "RewriteErrors": "on",
                 "DelayErrors": "on",
                 "DelayErrorsDuration": 1000,
-                "RequestType": "preprocessed", # The request type (preprocessed = non-xml) *required*
-                "ResponseType": "preprocessed", # The respone type (preprocessed = non-xml) *required*
+                "RequestType": str(request_type), # The request type (preprocessed = non-xml) *required*
+                "ResponseType": str(response_type), # The respone type (preprocessed = non-xml) *required*
                 "FollowRedirects": "on",
                 "RewriteLocationHeader": "off",
-                "StylePolicy": "Test_Policy", # The policy name *required*
-                "Type": "static-backend", # The type of the backend (default static-backend) *required*
+                "StylePolicy": str(policy_name), # The policy name *required*
+                "Type": "{0}-backend".format(str(backend_type)), # The type of the backend (default static-backend) *required*
                 "AllowCompression": "off",
                 "AllowCacheControlHeader": "off",
                 "PolicyAttachments": "Test", # ?
@@ -81,3 +74,10 @@ def create_mpgw(mpgw_name, fsh_name, backend_url, request_type, response_type, s
                 "TransactionTimeout": 0
             }
         }
+        link = str(config["datapower_rest_url"]) + "config/"+ str(domain_name) +"/MultiProtocolGateway"
+        response = requests.post(url=link, data=json.dumps(mpgw_object), auth=auth, verify=False)
+        click.echo("{0} -- {1}".format(response.status_code, response.reason))
+        if int(int(response.status_code) / 100) == 2:
+            click.secho('Success - MPGW {0} has been created!.'.format(mpgw_name), fg='green')
+        else:
+            click.secho('Failure - MPGW {0} could not been created! error: {1}.'.format(mpgw_name, response.json()['error']), fg='red')
