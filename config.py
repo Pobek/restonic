d41bc7388508f005b8b530f3a506b6a684507d1c
Sputters <coevyatar@gmail.com>
2018-10-22 00:57:06 +0300

    config.py: Change the example configuration & Add search functions
    1) The example configuration file that is being generated when there is no present configuration has been changed to generate the new configuration file.
    2) Added 2 new search functions to help getting the datapower's object and/or environment object from the configuration file with ease.

--- config.py
+++ config.py
@@ -23,15 +23,34 @@ class Config:
             click.secho("Could not find 'config_restornic.json' configuration. An example one has been created", fg="red")
             self.generate_config()
             return self.load_config()
-        
+
+    def get_environmet_from_dp_name(self, name):
+        for key, value in self.config.items():
+            if isinstance(value, list):
+                for item in value:
+                    if item["name"] == name:
+                        return value
+
+    def get_dp_object_from_dp_name(self, name):
+        for key, value in self.config.items():
+            if isinstance(value, list):
+                for item in value:
+                    if item["name"] == name:
+                        return item
+
     def generate_config(self):
         config_example = {
-            "comment" : "An example file",
-            "datapower_rest_url" : "https://localhost:5554/mgmt/",
-            "credentials" : {
-                "username" : "admin",
-                "password" : "admin"
-            }
+            "datapower_env_1" : [
+                {
+                    "name" : "dp1",
+                    "comment" : "Datapower dev 1",
+                    "credentials" : {
+                        "username" : "admin",
+                        "password" : "admin"
+                    },
+                    "datapower_rest_url" : "https://localhost:5554/mgmt/"
+                }
+            ]
         }
         with codecs.open(os.path.join(self.CONFIG_PATH, "config_restonic.json"), "w", "utf-8") as json_file:
             json.dump(config_example, json_file, sort_keys=True, indent=4, ensure_ascii=False)
