9597afc8e6ca3bc3c4d1ad675da07e9d9f8ba2b1
Sputters <coevyatar@gmail.com>
2018-10-23 20:50:56 +0300

    generic_utils.commands.py: Apply config changes to other commands

--- cli/__init__.py
+++ cli/__init__.py
@@ -3,7 +3,7 @@ from . import click
 #from .mpgw_commands import create_mpgw
 from .fsh_commands import create_http_fsh, create_mq_fsh 
 #from .mqqm_commands import create_mq_qm
-#from .generic_utils_commands import save_config
+from .generic_utils_commands import save_config
 #from .policy_commands import create_policy
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -16,5 +16,5 @@ def restonic():
 restonic.add_command(create_http_fsh)
 restonic.add_command(create_mq_fsh)
 #restonic.add_command(create_mq_qm)
-#restonic.add_command(save_config)
+restonic.add_command(save_config)
 #restonic.add_command(create_policy)
--- cli/generic_utils_commands.py
+++ cli/generic_utils_commands.py
@@ -4,21 +4,46 @@ import json
 import urllib3
 from config import Config
 
-config = Config().config
-auth = (config["credentials"]["username"], config["credentials"]["password"])
+config = Config()
 
 @click.command()
+@click.option('--dp-target', help="Set the target of the command. Could either be a single datapower, a list of datapowers. ")
+@click.option('--env-target', help="Set the target of the command. Could either be a single datapower environment, a list of datapower environments. ")
 @click.argument('domain_name')
-def save_config(domain_name):
+def save_config(domain_name, dp_target, env_target):
     """ This command creates an IBM MQ Front sider handler """
     click.secho("Saving Configuration", fg='yellow')
+
+    dp_object = None
+
+    if (not dp_target is None and not env_target is None) or (dp_target is None and not env_target is None):
+        dp_object = config.config[env_target]
+    elif not dp_target is None and env_target is None:
+        dp_object = config.get_dp_object_from_dp_name(dp_target)
+    else:
+        click.secho("The option '--dp-target' or '--env-target' must be initialized to use this command.", fg='red') 
+        return
+
     config_object = {
         "SaveConfig" : ""
     }
-    link = str(config["datapower_rest_url"]) + "actionqueue/"+ str(domain_name)
-    response = requests.post(url=link, data=json.dumps(config_object), auth=auth, verify=False)
-    click.echo("{0} -- {1}".format(response.status_code, response.reason))
-    if int(int(response.status_code) / 100) == 2:
-        click.secho('Success - Configuration saved.', fg='green')
-    else:
-        click.secho('Failure - Configuration not saved.', fg='red')
+    
+    if isinstance(dp_object, dict):
+        auth = (dp_object["credentials"]["username"], dp_object["credentials"]["password"])
+        link = str(dp_object["datapower_rest_url"]) + "actionqueue/"+ str(domain_name)
+        response = requests.post(url=link, data=json.dumps(config_object), auth=auth, verify=False)
+        click.echo("{0} -- {1}".format(response.status_code, response.reason))
+        if int(int(response.status_code) / 100) == 2:
+            click.secho('Success - Configuration saved.', fg='green')
+        else:
+            click.secho('Failure - Configuration not saved.', fg='red')
+    elif isinstance(dp_object, list):
+        for datapower in dp_object:
+            auth = (datapower["credentials"]["username"], datapower["credentials"]["password"])
+            link = str(datapower["datapower_rest_url"]) + "actionqueue/"+ str(domain_name)
+            response = requests.post(url=link, data=json.dumps(config_object), auth=auth, verify=False)
+            click.echo("{0} -- {1}".format(response.status_code, response.reason))
+            if int(int(response.status_code) / 100) == 2:
+                click.secho('Success - Configuration saved for {0}.'.format(datapower["name"]), fg='green')
+            else:
+                click.secho('Failure - Configuration not saved for {0}.'.format(datapower["name"]), fg='red')
