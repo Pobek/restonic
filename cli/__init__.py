af8db4edc4350a427bcfe2b84c273524fb9b1ed7
Sputters <coevyatar@gmail.com>
2018-10-23 21:42:20 +0300

    mqqm_commands.py: Apply config changes to other commands

--- cli/__init__.py
+++ cli/__init__.py
@@ -2,7 +2,7 @@ import urllib3
 from . import click
 from .mpgw_commands import create_mpgw
 from .fsh_commands import create_http_fsh, create_mq_fsh 
-#from .mqqm_commands import create_mq_qm
+from .mqqm_commands import create_mq_qm
 from .generic_utils_commands import save_config
 #from .policy_commands import create_policy
 
@@ -15,6 +15,6 @@ def restonic():
 restonic.add_command(create_mpgw)
 restonic.add_command(create_http_fsh)
 restonic.add_command(create_mq_fsh)
-#restonic.add_command(create_mq_qm)
+restonic.add_command(create_mq_qm)
 restonic.add_command(save_config)
 #restonic.add_command(create_policy)
--- cli/mqqm_commands.py
+++ cli/mqqm_commands.py
@@ -4,19 +4,31 @@ import json
 import urllib3
 from config import Config
 
-config = Config().config
-auth = (config["credentials"]["username"], config["credentials"]["password"])
+config = Config()
 
 @click.command()
 @click.option('--state', type=click.Choice(['enabled','disabled']), default="enabled", help='Set the state of the object', show_default=True)
+@click.option('--dp-target', help="Set the target of the command. Could either be a single datapower, a list of datapowers. ")
+@click.option('--env-target', help="Set the target of the command. Could either be a single datapower environment, a list of datapower environments. ")
 @click.argument('object-name')
 @click.argument('queue-manager-ip')
 @click.argument('queue-manager-port')
 @click.argument('queue-manager-name')
 @click.argument('domain_name')
-def create_mq_qm(object_name, queue_manager_ip, queue_manager_port, queue_manager_name, domain_name, state):
+def create_mq_qm(object_name, queue_manager_ip, queue_manager_port, queue_manager_name, domain_name, state, dp_target, env_target):
     """ This command creates an IBM MQ QueueManager Object """
     click.echo("Creating QM Object : {0}".format(object_name))
+    
+    dp_object = None
+
+    if (not dp_target is None and not env_target is None) or (dp_target is None and not env_target is None):
+        dp_object = config.config[env_target]
+    elif not dp_target is None and env_target is None:
+        dp_object = config.get_dp_object_from_dp_name(dp_target)
+    else:
+        click.secho("The option '--dp-target' or '--env-target' must be initialized to use this command.", fg='red') 
+        return
+
     mqqm_object = {
         "MQQM" : {
             "name" : str(object_name),
@@ -45,7 +57,16 @@ def create_mq_qm(object_name, queue_manager_ip, queue_manager_port, queue_manage
             "AlternateUser" : "on",
             "SSLClientConfigType" : "proxy"
         }
+
     }
-    link = str(config["datapower_rest_url"]) + "config/"+ str(domain_name) +"/MQQM"
-    response = requests.post(url=link, data=json.dumps(mqqm_object), auth=auth, verify=False)
-    click.echo("{0} -- {1}".format(response.status_code, response.reason))
+    if isinstance(dp_object, dict):
+        auth = (dp_object["credentials"]["username"], dp_object["credentials"]["password"])
+        link = str(dp_object["datapower_rest_url"]) + "config/"+ str(domain_name) +"/MQQM"
+        response = requests.post(url=link, data=json.dumps(mqqm_object), auth=auth, verify=False)
+        click.echo("{0} -- {1}".format(response.status_code, response.reason))
+    elif isinstance(dp_object, list):
+        for datapower in dp_object:
+            auth = (datapower["credentials"]["username"], datapower["credentials"]["password"])
+            link = str(datapower["datapower_rest_url"]) + "config/"+ str(domain_name) +"/MQQM"
+            response = requests.post(url=link, data=json.dumps(mqqm_object), auth=auth, verify=False)
+            click.echo("datapower : {0}, {1} -- {2}".format(datapower["name"],  response.status_code, response.reason))
