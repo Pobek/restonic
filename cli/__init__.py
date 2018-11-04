224ebb71ba98e56fc5c316e1227723cf10e2fab5
Sputters <coevyatar@gmail.com>
2018-11-04 22:45:47 +0200

    Add filestore functionallity

--- cli/__init__.py
+++ cli/__init__.py
@@ -5,6 +5,7 @@ from .fsh_commands import create_http_fsh, create_mq_fsh
 from .mqqm_commands import create_mq_qm
 from .generic_utils_commands import save_config
 from .policy_commands import create_policy
+from .filestore_commands import list_directory
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -18,3 +19,4 @@ restonic.add_command(create_mq_fsh)
 restonic.add_command(create_mq_qm)
 restonic.add_command(save_config)
 restonic.add_command(create_policy)
+restonic.add_command(list_directory)
---
+++ cli/filestore_commands.py
@@ -0,0 +1,65 @@
+from cli import click
+import requests
+import json
+import urllib3
+import codecs
+from config import Config
+
+config = Config()
+
+@click.command()
+@click.option('--dp-target', help="Set the target of the command. Could either be a single datapower, a list of datapowers. ")
+@click.option('--env-target', help="Set the target of the command. Could either be a single datapower environment, a list of datapower environments. ")
+@click.option('--file-target', help="Save the response content in a file")
+@click.argument('target_directory', help="The target directory. Needs to be in the format : '<dp_dir>/<target_dir>' or just '<dp_dir>/'")
+@click.argument('domain_name')
+def list_directory(target_directory, domain_name, dp_target, env_target, file_target):
+    """ This command lets you manipulate files from the datapower """
+
+    dp_object = None
+
+    if (not dp_target is None and not env_target is None) or (dp_target is None and not env_target is None):
+        dp_object = config.config[env_target]
+    elif not dp_target is None and env_target is None:
+        dp_object = config.get_dp_object_from_dp_name(dp_target)
+    else:
+        click.secho("The option '--dp-target' or '--env-target' must be initialized to use this command.", fg='red') 
+        return
+
+    if isinstance(dp_object, dict):
+        click.secho("Searching for directory '{0}'".format(target_directory))
+        auth = (dp_object["credentials"]["username"], dp_object["credentials"]["password"])
+        folder_split = target_directory.split("/")
+        if len(folder_split) > 1:
+            link = str(dp_object["datapower_rest_url"]) + "filestore/"+ str(domain_name) +"/"+ str(target_directory)
+        else:
+            link = str(dp_object["datapower_rest_url"]) + "filestore/"+ str(domain_name) +"/"+ str(target_directory)
+        click.echo(link)
+        response = requests.get(url=link, auth=auth, verify=False)
+        response_json = response.json()["filestore"]["location"]
+        click.echo("{0} -- {1}".format(response.status_code, response.reason))
+        if not file_target is None:
+            with codecs.open(file_target, 'w', 'utf-8') as w_file:
+                json.dump(response_json, w_file, sort_keys=True, indent=4, ensure_ascii=False)
+            click.secho("Response saved to : '{0}'".format(file_target), fg="green")
+        else:
+            click.echo("Content : {0}".format(json.dumps(response_json, sort_keys=True, indent=4)))
+    elif isinstance(dp_object, list):
+        for datapower in dp_object:
+            click.secho("Searching for directory '{0}' in datapower : {1}".format(target_directory, datapower["name"]))
+            auth = (datapower["credentials"]["username"], datapower["credentials"]["password"])
+            folder_split = target_directory.split("/")
+            if len(folder_split) > 1:
+                link = str(datapower["datapower_rest_url"]) + "filestore/"+ str(domain_name) +"/"+ str(target_directory)
+            else:
+                link = str(datapower["datapower_rest_url"]) + "filestore/"+ str(domain_name) +"/"+ str(target_directory)
+            click.echo(link)
+            response = requests.get(url=link, auth=auth, verify=False)
+            response_json = response.json()["filestore"]["location"]
+            click.echo("{0} -- {1}".format(response.status_code, response.reason))
+            if not file_target is None:
+                with codecs.open(file_target + "_" + datapower["name"], 'w', 'utf-8') as w_file:
+                    json.dump(response_json, w_file, sort_keys=True, indent=4, ensure_ascii=False)
+                click.secho("Response saved to : '{0}'".format(file_target), fg="green")
+            else:
+                click.echo("Datapower {0}, Content : {1}".format(datapower["name"],json.dumps(response_json, sort_keys=True, indent=4)))
